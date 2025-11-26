adb045fef7ad1560241e3f602e9d82b9219e8933
hanu-n <nanahsunuy@gmail.com>
2025-11-26 23:24:50 +0300

    prepare for deployment

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-   baseURL: "http://localhost:5000/api"
-  // "https://lms-vx2w.onrender.com"
+   baseURL: "https://lms-vx2w.onrender.com"
  ,
   
 });
